f1032cc25c1c826a90d2c97e86a58fbebdbf2102
Iman0810 <iman.chandrasiri0810@gmail.com>
2026-08-03 18:31:17 +0530

    cleaner updated code added #2

--- backend/src/main/java/backend/config/WebSocketConfig.java
+++ backend/src/main/java/backend/config/WebSocketConfig.java
@@ -21,4 +21,5 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(metricWebSocketHandler, "/ws")
                 .setAllowedOriginPatterns("*");
     }
+    
 }
