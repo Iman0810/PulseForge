a7cc85849146be08a409c497aad26065818e6989
Iman0810 <iman.chandrasiri0810@gmail.com>
2026-08-13 13:26:24 +0530

    Auth backend added

---
+++ backend/src/main/java/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package backend.repository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import backend.model.User;
+
+public interface UserRepository extends JpaRepository<User , Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+    
+} 
+    
+
