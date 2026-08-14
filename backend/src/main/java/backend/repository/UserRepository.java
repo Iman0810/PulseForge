cce58cdf084f43a746989d3fa1fb1edd51d1bf04
Iman0810 <iman.chandrasiri0810@gmail.com>
2026-08-14 20:00:04 +0530

    login endpoint added

--- backend/src/main/java/backend/repository/UserRepository.java
+++ backend/src/main/java/backend/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package backend.repository;
-import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import backend.model.User;
 
-public interface UserRepository extends JpaRepository<User , Long> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByUsername(String username);
-    
-} 
-    
+    User findByUsername(String username);
 
+}
