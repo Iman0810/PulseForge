8f4da507916ce225192b92d4667aff92649685e1
Iman0810 <iman.chandrasiri0810@gmail.com>
2026-08-24 08:12:49 +0530

    axios jwt interceptor updated

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,6 +4,7 @@ const api = axios.create({
     baseURL: "http://localhost:8080/api"
 });
 
+
 api.interceptors.request.use(
     (config) => {
 
@@ -11,18 +12,45 @@ api.interceptors.request.use(
 
         if (token) {
 
-            config.headers.Authorization = `Bearer ${token}`;
+            config.headers.Authorization =
+                `Bearer ${token}`;
 
         }
 
         return config;
-
     },
+
     (error) => {
 
         return Promise.reject(error);
 
     }
 );
+
+
+api.interceptors.response.use(
+
+    (response) => {
+
+        return response;
+
+    },
+
+    (error) => {
+
+        if (error.response?.status === 401) {
+
+            localStorage.removeItem("token");
+
+            window.location.href = "/login";
+
+        }
+
+        return Promise.reject(error);
+
+    }
+
+);
+
 
 export default api;
